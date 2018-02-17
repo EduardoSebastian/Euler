d761cc8782a714d4278221b79b5aa4318ed21fec
EduardoSebastian <32284851+EduardoSebastian@users.noreply.github.com>
2018-02-17 10:54:40 -0300

    Better

--- 2.js
+++ 2.js
@@ -1,10 +1,10 @@
 // Problem two
 var f = _ => {
   var a = b = 1, r = 0;
-  while(a < 4000000) {
+  while(a < 4e6) {
      a += b;
      b = a - b;
      r += !(a % 2) ? a : 0;
-     if( a >= 4000000) return r;
+     if( a >= 4e6) return r;
   }
 }
