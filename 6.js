4ef042634c31443313ceb1a83949c874484d2d3d
EduardoSebastian <32284851+EduardoSebastian@users.noreply.github.com>
2018-02-20 00:28:16 -0300

    Unnecessary loop

--- 6.js
+++ 6.js
@@ -1,10 +1,10 @@
 // Problem six
 function add() {
  var pow = 0, n = 100, sum = Math.pow(n * (n + 1)/2, 2);
- for(var i = 0; i <= 100; ++i) {
+ for(var i = 2; i <= 100; ++i) {
    pow += i * i;
  }
- return sum - pow;
+ return sum - (pow + 1);
 }
 
 console.log(add());
