3141cd1c98db889c74e5a58858017b24ee620cf7
EduardoSebastian <32284851+EduardoSebastian@users.noreply.github.com>
2018-02-21 01:42:28 -0300

    Much better !!!
    1300 ms , now to 350 ms !

--- 7.js
+++ 7.js
@@ -1,12 +1,10 @@
 /* Problem seven
 The solution will be more efficient in the future (when i have more knowledge about this algorithm)
 */
-var start = performance.now();
-
 function eratosthenes(n) {
   var arr = [2], acc = 0;
   for(var i = 3; true; i += 2) {
-    if(arr.length === n) return i;
+    if(arr.length === n) return i - 2;
     if(!resolve(arr, i)) {
       arr.push(i);
     }
@@ -14,7 +12,11 @@ function eratosthenes(n) {
 }
 
 function resolve(array, n) {
-  return array.some(cur => !(n%cur));
+  for(var i = 0, l = array.length - 1; i <= l; ++i) {
+    var cur = array[i];
+    if(!(n%cur)) return true;
+  }
+  return false;
 }
 
-eratosthenes(10001); // Result ( Tooks 1300 ms )
+eratosthenes(10001); // Result ( Tooks 350 ms )
