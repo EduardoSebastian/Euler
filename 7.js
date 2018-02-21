fb0de19b82f52ef4c93d735c9fac13c1719b459b
EduardoSebastian <32284851+EduardoSebastian@users.noreply.github.com>
2018-02-21 00:26:24 -0300

    Better

--- 7.js
+++ 7.js
@@ -1,20 +1,22 @@
-// Problem seven
-function setPrime(n) {
-  var acc = 0;
-  for(var i = 2; true; ++i) {
-    if(check(i)) acc++;
-    if(acc === 10001) {
-      return acc;
+/* Problem seven
+The solution will be more efficient in the future (when i have more knowledge about this algorithm)
+*/
+var start = performance.now();
+
+function eratosthenes(n) {
+  var arr = [2], acc = 0;
+  for(var i = 3; true; i += 2) {
+    if(arr.length === n) return i;
+    if(!resolve(arr, i)) {
+      arr.push(i);
     }
   }
 }
 
-function check(n) {
-  for(var i = 2; i < n; ++i) {
-    if(!(n%i)) return false;
-  }
-  return true;
+function resolve(array, n) {
+  return array.filter(cur =>  {
+    return !(n%cur);
+  }).length > 0;
 }
 
-
-console.log(setPrime());
+eratosthenes(10001); // Result ( Tooks 2800 ms )
